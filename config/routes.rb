dffb1301a696a82fe12675c58e0cc4deb9a3a42a
宮本 涼太郎 <ryotaro_miyamoto@achikinoSurface.local>
2024-02-05 18:19:24 +0900

    logins,looutsコントローラ削除、sessionsコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :times
+  resource :logins, only: :new
 
   get "up" => "rails/health#show", as: :rails_health_check 
   #ヘルスチェック用ルーティング　残す 
