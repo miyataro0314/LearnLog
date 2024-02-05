e8fc756b3e81778ebe3bb21609e70065bb5f6475
宮本 涼太郎 <ryotaro_miyamoto@achikinoSurface.local>
2024-02-05 18:09:52 +0900

    logins,logoutsコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/' => "main#learnlog"
 
   resources :users
+  resources :times
 
   get "up" => "rails/health#show", as: :rails_health_check 
   #ヘルスチェック用ルーティング　残す 
